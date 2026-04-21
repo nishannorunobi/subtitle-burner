1225dcbd93c790a4121f1800405368806028fcfa
Nishan <norunnabinishan@gmail.com>
2026-04-21 19:36:04 -0400

    update osenv

--- osenv.sh
+++ osenv.sh
@@ -6,5 +6,5 @@ apt install -y software-properties-common
 add-apt-repository -y ppa:deadsnakes/ppa
 apt update
 apt install -y python3.10 python3.10-venv python3.10-distutils python3-pip
-sudo apt install -y ffmpeg fonts-noto
+apt install -y ffmpeg fonts-noto
 
